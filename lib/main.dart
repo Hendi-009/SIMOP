f233113099f99b98e2cc06ede642f54b9d419bba
Hendi <hendychairul9@gmail.com>
2023-05-11 17:35:44 +0800

    penambahan theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
-import 'package:simop/screen/splash/splash.dart';
+import 'package:pa_simop/screen/splash/splash.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
-import 'package:simop/theme/theme_notifier.dart';
 import 'package:provider/provider.dart';
+import 'package:pa_simop/theme/theme_notifier.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
---
+++ lib/theme/theme_notifier.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+enum ThemeMode { light, dark }
+
+class ThemeNotifier extends ChangeNotifier {
+  ThemeData defaultTheme = ThemeData(
+    primarySwatch: Colors.green,
+    fontFamily: 'Poppins',
+  );
+  ThemeData _lightTheme = ThemeData.light().copyWith(
+    scaffoldBackgroundColor: Color(0xFFFCFCFC),
+    textTheme: const TextTheme(
+      titleLarge: TextStyle(
+          fontSize: 18, fontWeight: FontWeight.w600, color: Color(0xFF65A25E)),
+      titleMedium: TextStyle(
+          fontSize: 16, fontWeight: FontWeight.w500, color: Color(0xFF65A25E)),
+      titleSmall: TextStyle(
+          fontSize: 14, fontWeight: FontWeight.w400, color: Color(0xFF65A25E)),
+      headlineLarge: TextStyle(
+          fontSize: 14, fontWeight: FontWeight.w500, color: Color(0xFF111111)),
+      bodyLarge: TextStyle(
+          fontSize: 14, fontWeight: FontWeight.w400, color: Color(0xFF111111)),
+      bodyMedium: TextStyle(
+          fontSize: 14, fontWeight: FontWeight.w400, color: Color(0xFF777777)),
+      bodySmall: TextStyle(
+          fontSize: 12, fontWeight: FontWeight.w400, color: Color(0xFF111111)),
+      labelMedium: TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+        color: Color(0xFFFCFCFC),
+      ),
+      labelSmall: TextStyle(
+          fontSize: 12, fontWeight: FontWeight.w400, color: Color(0xFF65A25E)),
+    ),
+  );
+  ThemeData _darkTheme = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: const TextTheme(
+      titleLarge: TextStyle(
+          fontSize: 18, fontWeight: FontWeight.w600, color: Color(0xFF65A25E)),
+      titleMedium: TextStyle(
+          fontSize: 16, fontWeight: FontWeight.w500, color: Color(0xFF65A25E)),
+      titleSmall: TextStyle(
+          fontSize: 14, fontWeight: FontWeight.w400, color: Color(0xFF65A25E)),
+      headlineLarge: TextStyle(
+          fontSize: 14, fontWeight: FontWeight.w500, color: Color(0xFF111111)),
+      bodyLarge: TextStyle(
+          fontSize: 14, fontWeight: FontWeight.w400, color: Color(0xFF111111)),
+      bodyMedium: TextStyle(
+          fontSize: 14, fontWeight: FontWeight.w400, color: Color(0xFF777777)),
+      bodySmall: TextStyle(
+          fontSize: 12, fontWeight: FontWeight.w400, color: Color(0xFF111111)),
+      labelMedium: TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+        color: Color(0xFFFCFCFC),
+      ),
+      labelSmall: TextStyle(
+          fontSize: 12, fontWeight: FontWeight.w400, color: Color(0xFF65A25E)),
+    ),
+  );
+
+  ThemeData _currentTheme = ThemeData.light();
+
+  ThemeData get currentTheme => _currentTheme;
+
+  ThemeMode _currentMode = ThemeMode.light;
+
+  ThemeMode get currentMode => _currentMode;
+
+  void toggleTheme() {
+    _currentMode =
+        _currentMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    _currentTheme = _currentMode == ThemeMode.light ? _lightTheme : _darkTheme;
+    notifyListeners();
+  }
+}
